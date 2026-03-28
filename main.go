c65efc1450c23fba13e15a2d6e4613f72403ba97
emirdevback <onsekiznitro@gmail.com>
2026-03-28 17:15:59 +0300

    iki endpoint eklendi

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 )
 
+var urlMap = make(map[string]string)
+
 func main() {
+
 	fmt.Println("Sunucu başlatılıyor...")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "URL Kısaltıcıya Hoşgeldiniz!")
 
+	})
+
+	http.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Link kısaltma endpoint'i")
 	})
 
 	http.ListenAndServe(":8080", nil)
